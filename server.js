35d9f4480a05bb6a470714d7bd0b0e6f8338087c
Nitesh Wakhare <nitesh.wakhare@intellectdesign.com>
2018-03-21 17:10:59 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('sever.log', log + '\n', (err)=> {
+    console.log(err);
+    next();
+  });
+});
+
+// app.use((req,res,next) => {
+//   res.render('maintenance.hbs');
+// })
+
+hbs.registerHelper('getCurrentYear', () =>{
+  return new Date().getFullYear();
+}
+)
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+app.get('/',(req, res)=>{
+  // res.send('<h1>Hello Express!</h1>');
+  res.render("home.hbs", {
+    pageTitle: 'Home page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/about',(req, res)=>{
+  res.render("about.hbs", {
+    pageTitle: 'About page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage : 'Unable to handle request. Error page'
+  })
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000.');
+});
